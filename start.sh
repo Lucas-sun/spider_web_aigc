347af85b14bed868b785a54af1ab7794db0dc551
lucas.sun <lucas.sun@mirmiles.com>
2025-10-29 16:10:54 +0800

    debug: 启动脚本

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
-pwd
+python
 
-source /home/mmt/anaconda3/etc/profile.d/conda.sh
-
-conda activate .venv_py311
-
-pip install -r ./requirements.txt
-
-python main.py --crawl_aigc 1
+#pip install -r ./requirements.txt
+#
+#python main.py --crawl_aigc 1
